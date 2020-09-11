40fdb185b0784fa7b16606313ad26e590b71a42b
Foiros <56882322+Foiros@users.noreply.github.com>
2020-09-11 12:09:02 +0300

    Arttu Paldan Commit I 11.9.2020
    Made abstract class for the weapon components, set up the needed objects in unity and started doing BuyComponent script.

---
+++ Assets/Scripts/WeaponUpgrade/AbstractWeaponComponent.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class AbstractWeaponComponent : MonoBehaviour
+{
+    private int compomnentID;
+
+    private int componentCost;
+
+    private SpriteRenderer componentNotBougthImage;
+
+    private SpriteRenderer componentBoughtImage;
+
+    private SpriteRenderer placedComponentImage;
+
+    int GetID(int compID)
+    {
+        compomnentID = compID;
+        return compomnentID;
+    }
+
+    int GetCost(int cost)
+    {
+        componentCost = cost;
+        return componentCost;
+    }
+
+    SpriteRenderer GetNotBoughtImage(SpriteRenderer notBought)
+    {
+        componentNotBougthImage = notBought;
+        return componentNotBougthImage;
+    }
+
+    SpriteRenderer GetBoughtImage(SpriteRenderer bought)
+    {
+        componentBoughtImage = bought;
+        return componentBoughtImage;
+    }
+
+    SpriteRenderer GetPlacedImage(SpriteRenderer placed)
+    {
+        placedComponentImage = placed;
+        return placedComponentImage;
+    }
+
+    public virtual void ComponentEffect()
+    {
+    }
+}
