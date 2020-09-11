57bae820ed59db6a198464a0e45d8ad780f7b4cb
qbao500 <54832882+qbao500@users.noreply.github.com>
2020-09-11 15:14:55 +0300

    Bao commit 1 11.09
    Enemy movement and spawning

---
+++ Assets/Bao_Enemy/EnemyMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        // Enemy move left direction
+        transform.Translate(Time.deltaTime * speed * -1, 0, 0);
+    }
+}
