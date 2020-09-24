0218c17c7a1414a1a9061fe976a6d0df49dc8c37
StonegazeFIN <33735539+StonegazeFIN@users.noreply.github.com>
2020-09-24 18:05:36 +0300

    Healthbar updates and a moving platform.

--- Assets/Scripts/Player/PlayerStat.cs
+++ Assets/Scripts/Player/PlayerStat.cs
@@ -8,6 +8,7 @@ public class PlayerStat : MonoBehaviour
    
     
     public GameObject player;
+    public Healthbar_Ossi healthBar;
    
     public int PlayerHP;
     public int PlayerMaxHP;
@@ -18,19 +19,57 @@ public class PlayerStat : MonoBehaviour
 
     void Start()
     {
-
+        healthBar = GetComponentInChildren<Healthbar_Ossi>();
+        if(healthBar != null)
+        {
+            Debug.Log("PlayerHP bar found.");
+        }
         PlayerHP = PlayerMaxHP;
         
     }
     void Update()
     {
 
-        if (Input.GetKeyDown(KeyCode.X))    
+        //if (Input.GetKeyDown(KeyCode.X))    
+        //{
+        //    PlayerHP -= 30;
+        //}
+
+        //Ossi's version:
+        if(Input.GetKeyDown(KeyCode.Y))
         {
-            PlayerHP -= 30;
+            updateHealth(10);
+        }
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            updateHealth(-10);
         }
 
 
+    }
+
+    //Ossi's take on player HP change
+    public void updateHealth(int changeHP)
+    {
+        PlayerHP += changeHP;
+        if(PlayerHP > PlayerMaxHP)
+        {
+            //This is make sure that the Healthbar component isn't unnecessarily called.
+            PlayerHP = PlayerMaxHP;
+        } 
+        else
+        {
+        if(PlayerHP <= 0)
+            {
+                // This checks if the player takes fatal damage.
+                PlayerHP = 0;
+                Debug.Log("Dead.");
+            }
+        //Add the new health values to change the fill amount of the healthbar.
+            healthBar.updateHealthBar(PlayerMaxHP, PlayerHP);
+        }    
+        
+        
     }
 
 
