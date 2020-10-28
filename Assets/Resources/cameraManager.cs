0e122906158517fef516bd4d978c56db49c82c41
StonegazeFIN <33735539+StonegazeFIN@users.noreply.github.com>
2020-10-28 17:00:41 +0200

    Camera zooms in and out
    With the CineMachine asset the camera will follow the player and has 2 separate cameras for zoom In and zoom Out.

---
+++ Assets/Resources/cameraManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraManager : MonoBehaviour
+{
+    public GameObject zoomIn, zoomOut;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Zoom();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            Zoom();
+        }
+    }
+
+    public void Zoom()
+    {
+        if(zoomIn.activeSelf == true)
+        {
+            zoomOut.SetActive(true);
+            zoomIn.SetActive(false);
+        } else
+        {
+            zoomOut.SetActive(false);
+            zoomIn.SetActive(true);
+        }
+
+    }
+}
