860434851083517fdc79cfb351cafb324a83378a
Foiros <56882322+Foiros@users.noreply.github.com>
2020-11-05 15:31:35 +0200

    Arttu Commit III 5.11.2020
    Dialogue system finished. Gonna add visual flare later, now it''s just a textmeshpro which switches messages.

---
+++ Assets/Scripts/Dialogue/PlayerSpeech.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+// Created by Arttu Paldán on 5.11.2020: 
+public class PlayerSpeech: MonoBehaviour
+{
+    GameObject bubbleObject;
+    TextMeshProUGUI speechBubble;
+
+    public string howToAttack, howToBlock;
+    public float messageTime;
+
+    void Awake()
+    {
+        bubbleObject = GameObject.FindGameObjectWithTag("PlayerSpeechBubble");
+        speechBubble = bubbleObject.GetComponent<TextMeshProUGUI>();
+    }
+
+    void Start()
+    {
+        speechBubble.text = "";
+
+        StartCoroutine(Wait(messageTime));
+    }
+
+    IEnumerator Wait(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        StartCoroutine(SpawnMessage(speechBubble, howToAttack, howToBlock, messageTime));
+    }
+
+    IEnumerator SpawnMessage(TextMeshProUGUI messageObject, string message1, string message2, float time)
+    {
+        messageObject.text = message1;
+
+        yield return new WaitForSeconds(time);
+
+        messageObject.text = message2;
+
+        yield return new WaitForSeconds(time);
+
+        messageObject.text = "";
+    }
+}
