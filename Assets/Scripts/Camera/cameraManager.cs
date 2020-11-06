22cf0fa79e2e859aaa0a8a527cb4fb94fd735326
Foiros <56882322+Foiros@users.noreply.github.com>
2020-11-06 13:58:11 +0200

    Revert "Revert "Revert "Camera updates and tweaks"""
    This reverts commit 91e4734280fe3c58859cd1242a9d4ba2b70b213c.

--- Assets/Scripts/Camera/cameraManager.cs
+++ Assets/Scripts/Camera/cameraManager.cs
@@ -5,63 +5,35 @@ using Cinemachine;
 
 public class cameraManager : MonoBehaviour
 {
-    //Made by Ossi Uusitalo
-    CinemachineVirtualCamera cineVirtual; // Main CineMachine component
-    CinemachineFramingTransposer cineComp; // This where we get the tracker's offset
-    public float top,maxSize; // These will mark the max values for the ortographic size (how large the camera view is) and maximum offset for the tracker. The camera will be on its maximum values in editor before shifting to zooming in.
-    public float bottom, minSize; //You need to manually set the CineMancer camera to the desired zoomed in camera and type down the Size and offset in the editor.
-    public float zoomMultiplier; //This will dictate how much zoom is applied on the mouse wheel scroll. Each tick of a mouse wheel is 1, which is then multiplied by this float.
-    public GameObject mainCamera;
-    
+    public GameObject zoomIn, zoomOut;
+    public bool isZoomed = true;
     // Start is called before the first frame update
-
-    private void Start()
-    {
-        Initialize();
-    }
-
-    private void Initialize()
-    {
-       cineVirtual = mainCamera.GetComponent<CinemachineVirtualCamera>();
-            if(cineVirtual != null)
-            {
-                Debug.Log("CineVirtual found");
-                cineComp = cineVirtual.GetCinemachineComponent<CinemachineFramingTransposer>();
-                if (cineComp != null)
-                {
-                    top = cineComp.m_ScreenY;
-                    maxSize = cineVirtual.m_Lens.OrthographicSize;
-                    cineComp.m_ScreenY = bottom;
-                    cineVirtual.m_Lens.OrthographicSize = minSize;
-                Debug.Log("CineComp found");
-                }
-            }
-    }
 
     void Update()
     {
-        cineComp.m_ScreenY -= Input.mouseScrollDelta.y / 100 * zoomMultiplier;
-        cineVirtual.m_Lens.OrthographicSize -= Input.mouseScrollDelta.y * zoomMultiplier;
-        if(cineVirtual.m_Lens.OrthographicSize < minSize)
+        if(isZoomed)
         {
-            cineVirtual.m_Lens.OrthographicSize = minSize;
+            zoomIn.gameObject.GetComponent<CinemachineVirtualCamera>().Priority = 15;
+        } else
+        {
+            zoomIn.gameObject.GetComponent<CinemachineVirtualCamera>().Priority = 5;
         }
 
-        if(cineVirtual.m_Lens.OrthographicSize > maxSize)
+        if(Input.GetKeyDown(KeyCode.C))
         {
-            cineVirtual.m_Lens.OrthographicSize = maxSize;
+            Zoom();
         }
+    }
 
-
-        if(cineComp.m_ScreenY > top)
+    public void Zoom()
+    {
+        if(isZoomed)
         {
-            cineComp.m_ScreenY = top;
-        }
-        if(cineComp.m_ScreenY < bottom)
+            isZoomed = false;
+        } else
         {
-            cineComp.m_ScreenY = bottom;
+            isZoomed = true;
         }
 
     }
-
 }
